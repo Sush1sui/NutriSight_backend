a54d0fde62683b451943dd8ce8007ba7737246d2
Sush1sui <jplovesraw@gmail.com>
2025-06-25 13:18:45 +0800

    tried changing callback url

--- src/middleware/passport.ts
+++ src/middleware/passport.ts
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/auth/google/callback",
+      callbackURL:
+        "https://nutrisight-backend.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
     async (
