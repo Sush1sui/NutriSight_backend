39f9f9fcef80133612d171d077bd46f748398e35
Sush1sui <jplovesraw@gmail.com>
2025-07-27 18:16:09 +0800

    refactor: processing of ingredients format written in backend

--- src/controllers/cameraController.ts
+++ src/controllers/cameraController.ts
@@ -40,13 +40,18 @@ export async function barcodeHandler(req: Request, res: Response) {
       return;
     }
 
-    const foodNutrients = food.foodNutrients.map((n: any) => {
-      return {
-        name: n.nutrientName,
-        amount: n.value,
-        unit: n.unitName,
-      };
-    });
+    const foodNutrients = chunkArray(
+      renameNutrition(
+        food.foodNutrients.map((n: any) => {
+          return {
+            name: n.nutrientName,
+            amount: n.value,
+            unit: n.unitName,
+          };
+        })
+      ),
+      3
+    );
 
     res.status(200).json({
       message: "Barcode data received successfully",
@@ -63,4 +68,53 @@ export async function barcodeHandler(req: Request, res: Response) {
     res.status(500).json({ message: "Internal server error" });
   }
   return;
+}
+
+function renameNutrition(arr: any[]) {
+  return arr.map((item: any) => {
+    if (
+      (item.name as string).toLowerCase() === "fatty acids, total saturated"
+    ) {
+      return { ...item, name: "Saturated Fats" };
+    }
+    if ((item.name as string).toLowerCase() === "fatty acids, total trans") {
+      return { ...item, name: "Trans Fats" };
+    }
+    if (
+      (item.name as string).toLowerCase() ===
+      "vitamin d (d2 + d3), international units"
+    ) {
+      return { ...item, name: "Vitamin D2 + D3" };
+    }
+    if ((item.name as string).toLowerCase() === "potassium, k") {
+      return { ...item, name: "Potassium" };
+    }
+    if ((item.name as string).toLowerCase() === "sodium, na") {
+      return { ...item, name: "Sodium" };
+    }
+    if ((item.name as string).toLowerCase() === "calcium, ca") {
+      return { ...item, name: "Calcium" };
+    }
+    if ((item.name as string).toLowerCase() === "iron, fe") {
+      return { ...item, name: "Iron" };
+    }
+    if ((item.name as string).toLowerCase() === "fiber, total dietary") {
+      return { ...item, name: "Dietary Fiber" };
+    }
+    if ((item.name as string).toLowerCase() === "total sugars") {
+      return { ...item, name: "Sugar" };
+    }
+    if ((item.name as string).toLowerCase() === "carbohydrate, by difference") {
+      return { ...item, name: "Carbohydrates" };
+    }
+    return item;
+  });
+}
+
+function chunkArray<T>(arr: T[], size: number): T[][] {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
 }
