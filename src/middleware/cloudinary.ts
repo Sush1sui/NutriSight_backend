3ca569ddf368de9a01e2187a2e1195531013da0c
Sush1sui <jplovesraw@gmail.com>
2025-07-27 13:34:23 +0800

    fix: dependencies fixed, cloudinary downgrade to v1

--- src/middleware/cloudinary.ts
+++ src/middleware/cloudinary.ts
@@ -1,15 +1,15 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary.v2,
   params: async (_req, _file) => ({
     folder: "user_profiles",
     allowed_formats: ["jpg", "png", "jpeg"],
