68575c4722aa641faf14ecb097d8e4ae32767f15
Sush1sui <jplovesraw@gmail.com>
2025-08-02 19:37:04 +0800

    refactor: removed dupe parsers

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ let timeoutId: NodeJS.Timeout;
 
 const app = express();
 app.set("trust proxy", 1); // Trust the first proxy
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "25mb" })); // Increase limit as needed
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
