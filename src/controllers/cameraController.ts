fe44b8f8de2740bd50186b3f656eb43b2c6f11c2
Sush1sui <jplovesraw@gmail.com>
2025-08-04 15:33:54 +0800

    test: filter nutrients that have greater than 0 values

--- src/controllers/cameraController.ts
+++ src/controllers/cameraController.ts
@@ -206,44 +206,48 @@ export async function foodScanHandler(req: Request, res: Response) {
 }
 
 function renameNutrition(arr: any[]) {
-  return arr.map((item: any) => {
-    if (
-      (item.name as string).toLowerCase() === "fatty acids, total saturated"
-    ) {
-      return { ...item, name: "Saturated Fats" };
-    }
-    if ((item.name as string).toLowerCase() === "fatty acids, total trans") {
-      return { ...item, name: "Trans Fats" };
-    }
-    if (
-      (item.name as string).toLowerCase() ===
-      "vitamin d (d2 + d3), international units"
-    ) {
-      return { ...item, name: "Vitamin D2 + D3" };
-    }
-    if ((item.name as string).toLowerCase() === "potassium, k") {
-      return { ...item, name: "Potassium" };
-    }
-    if ((item.name as string).toLowerCase() === "sodium, na") {
-      return { ...item, name: "Sodium" };
-    }
-    if ((item.name as string).toLowerCase() === "calcium, ca") {
-      return { ...item, name: "Calcium" };
-    }
-    if ((item.name as string).toLowerCase() === "iron, fe") {
-      return { ...item, name: "Iron" };
-    }
-    if ((item.name as string).toLowerCase() === "fiber, total dietary") {
-      return { ...item, name: "Dietary Fiber" };
-    }
-    if ((item.name as string).toLowerCase() === "total sugars") {
-      return { ...item, name: "Sugar" };
-    }
-    if ((item.name as string).toLowerCase() === "carbohydrate, by difference") {
-      return { ...item, name: "Carbohydrates" };
-    }
-    return item;
-  });
+  return arr
+    .filter((item: any) => item.value >= 0)
+    .map((item: any) => {
+      if (
+        (item.name as string).toLowerCase() === "fatty acids, total saturated"
+      ) {
+        return { ...item, name: "Saturated Fats" };
+      }
+      if ((item.name as string).toLowerCase() === "fatty acids, total trans") {
+        return { ...item, name: "Trans Fats" };
+      }
+      if (
+        (item.name as string).toLowerCase() ===
+        "vitamin d (d2 + d3), international units"
+      ) {
+        return { ...item, name: "Vitamin D2 + D3" };
+      }
+      if ((item.name as string).toLowerCase() === "potassium, k") {
+        return { ...item, name: "Potassium" };
+      }
+      if ((item.name as string).toLowerCase() === "sodium, na") {
+        return { ...item, name: "Sodium" };
+      }
+      if ((item.name as string).toLowerCase() === "calcium, ca") {
+        return { ...item, name: "Calcium" };
+      }
+      if ((item.name as string).toLowerCase() === "iron, fe") {
+        return { ...item, name: "Iron" };
+      }
+      if ((item.name as string).toLowerCase() === "fiber, total dietary") {
+        return { ...item, name: "Dietary Fiber" };
+      }
+      if ((item.name as string).toLowerCase() === "total sugars") {
+        return { ...item, name: "Sugar" };
+      }
+      if (
+        (item.name as string).toLowerCase() === "carbohydrate, by difference"
+      ) {
+        return { ...item, name: "Carbohydrates" };
+      }
+      return item;
+    });
 }
 
 function chunkArray<T>(arr: T[], size: number): T[][] {
