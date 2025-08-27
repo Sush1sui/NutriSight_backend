fa968417691cfc7775e3e11076b95ae4527cdcb0
Sush1sui <jplovesraw@gmail.com>
2025-08-27 16:30:20 +0800

    refactor: change url

--- src/utils/ingredientsPredict.ts
+++ src/utils/ingredientsPredict.ts
@@ -1,5 +1,5 @@
 const GEMINI_API_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-latest:generateContent";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 if (!GEMINI_API_KEY) {
   console.error("GEMINI_API_KEY is not set in the environment variables.");
