9570a7df7121cd222d596d523abf43d6e2874c78
Sush1sui <jplovesraw@gmail.com>
2025-09-05 16:41:59 +0800

    refactor: trying bigger model from hugging face

--- src/controllers/cameraController.ts
+++ src/controllers/cameraController.ts
@@ -9,8 +9,8 @@ import {
 } from "../utils/ingredientsNutritionsPredict";
 import FoodModel from "../models/Foods";
 import { convertToGrams } from "../utils/convertToGrams";
-import * as fs from "fs";
-import { classifyImage } from "../utils/model_inference";
+// import { classifyImage } from "../utils/model_inference";
+// import * as fs from "fs";
 
 const USDA_API_KEY = process.env.USDA_API_KEY;
 if (!USDA_API_KEY) {
@@ -30,10 +30,16 @@ if (!NUTRITIONIX_API_KEY) {
   process.exit(1);
 }
 
-const classNames = JSON.parse(
-  fs.readFileSync("src/cnn_model/class_names.json", "utf8")
-);
-const modelPath = "src/cnn_model/model.onnx";
+const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY;
+if (!HUGGINGFACE_API_KEY) {
+  console.error("HUGGINGFACE_API_KEY is not set in the environment variables.");
+  process.exit(1);
+}
+
+// const classNames = JSON.parse(
+//   fs.readFileSync("src/cnn_model/class_names.json", "utf8")
+// );
+// const modelPath = "src/cnn_model/model.onnx";
 
 export async function barcodeHandler(req: Request, res: Response) {
   try {
@@ -262,21 +268,44 @@ export async function predictFoodHandler(req: Request, res: Response) {
       return;
     }
 
-    // crop image
-
-    let imgBuffer: Buffer<ArrayBufferLike> | null = Buffer.from(
-      image,
-      "base64"
+    // send image to hugging face for inference api
+    const hfRes = await fetch(
+      "https://api-inference.huggingface.co/models/nateraw/food",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
+          "Content-Type": "application/octet-stream",
+        },
+        body: image,
+      }
     );
 
-    const predictions = await classifyImage(
-      imgBuffer,
-      modelPath,
-      classNames,
-      5
-    );
+    if (!hfRes.ok) {
+      const errBody = await hfRes.text();
+      console.error("Hugging Face API error:", errBody);
+      res.status(500).json({
+        error: "Failed to fetch data from Hugging Face",
+        message: errBody,
+      });
+      return;
+    }
 
-    imgBuffer = null;
+    const predictions = (await hfRes.json()) as any[];
+
+    // let imgBuffer: Buffer<ArrayBufferLike> | null = Buffer.from(
+    //   image,
+    //   "base64"
+    // );
+
+    // const predictions = await classifyImage(
+    //   imgBuffer,
+    //   modelPath,
+    //   classNames,
+    //   5
+    // );
+
+    // imgBuffer = null;
 
     if (!predictions || predictions.length === 0) {
       console.error("No food items detected in the image");
