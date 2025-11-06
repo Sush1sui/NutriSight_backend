0b3d88e0e49d2a65ab6c1cba1e4d79ae108e5108
Sush1sui <jplovesraw@gmail.com>
2025-11-06 13:58:17 +0800

    feat: get all food classes route

--- src/routes/camera.ts
+++ src/routes/camera.ts
@@ -4,8 +4,13 @@ import {
   getFoodDataHandler,
   predictFoodHandler,
 } from "../controllers/cameraController";
+import class_names from "../cnn_model/class_names.json";
 
 const router = Router();
+
+router.get("/food-classes", (_req, res) => {
+  return res.json(class_names);
+});
 
 router.post("/barcode", barcodeHandler);
 router.post("/predict-food", predictFoodHandler);
